10827f559a172e508a2b425f3c6efe11c74685c5
Kittipong Puankunta <proSbeginner@hotmail.com>
2019-07-28 01:45:10 +0700

    set Angular proxy configuration and update login template for send email and password to backend

--- boopack-frontend-part1/boopack/src/app/app.module.ts
+++ boopack-frontend-part1/boopack/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- boopack-frontend-part1/boopack/src/app/login/login.component.ts
+++ boopack-frontend-part1/boopack/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: [''],
+      password: [''],
+    });
   }
 
+  onLogin() {
+    const e = this.form.controls.email.value.trim();
+    const p = this.form.controls.password.value.trim();
+
+    const credentials = {
+      username: e,
+      password: p,
+    };
+    const url = `api/login`;
+
+    this.http.post(url, credentials).toPromise().then(user => {
+      console.log(user);
+    });
+  }
 }
